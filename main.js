70ad1069b4326867f5b5ee103d4b868a285d8d28
Keating <keatingrowe@gmail.com>
2022-10-20 13:18:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+
+  $("img.rat1").click(function () {
+    $("img.rat1").fadeOut();
+  });
+  $("img.rat2").click(function () {
+    $("img.rat2").fadeOut();
+  });
+
+});
